ecf9db7697c636a63f28df3dfb8d0859a921d3c9
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-03-08 17:17:23 -0500

    Initial Commit

---
+++ MoviesRatingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesRatingApp
+//
+//  Created by Abdulaziz Albahar on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesRatingApp/MoviesRatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesRatingAppApp.swift
+//  MoviesRatingApp
+//
+//  Created by Abdulaziz Albahar on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesRatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
