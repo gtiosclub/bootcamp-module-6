d23fab02e83dbfda6687e0e2999538a9561032c5
Hartej Singh <150478887+hartejsingh12@users.noreply.github.com>
2024-10-15 12:19:19 -0400

    Update RatingCardView.swift

--- MoviesRatingApp/TODO/RatingCardView.swift
+++ MoviesRatingApp/TODO/RatingCardView.swift
@@ -1,27 +1,13 @@
 import SwiftUI
 
-/**
- RATINGCARDVIEW.SWIFT
- */
 struct RatingCardView: View {
-    /**
-     INSTANCE VARIABLES
-     - Movie/Rating to be displayed on this card. ( Movie )
-     */
-    
-    // ADD CODE HERE
-    
-    /**
-     YOUR UI
-     - The actual implementation of a Card's UI will go below.
-     - Since leaving a text review is optional, you will need a conditional text depending on whether or not
-       the rating has a text review (evaluate the "myReview" field of the movie)
-     */
+    var movie: Movie
     
     var body: some View {
-        Text("YOUR UI HERE") // REPLACE WITH YOUR CODE
+        // UI Code Goes Here
     }
 }
+
 
 #Preview {
     RatingCardView() // Once you have instance variables, you will need to add arguments here to see preview.
