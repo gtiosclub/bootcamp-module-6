732e230fc6a2028965901482f6733cf6e195c023
Hartej Singh <150478887+hartejsingh12@users.noreply.github.com>
2024-10-15 12:17:23 -0400

    Update MoviesView.swift

--- MoviesRatingApp/TODO/MoviesView.swift
+++ MoviesRatingApp/TODO/MoviesView.swift
@@ -1,68 +1,28 @@
 import SwiftUI
 
-/**
- CONTENTVIEW.SWIFT
- - This is the landing page of your app. All subviews originate from here. You can change the landing page in the "MovieRatings_ShellCodeApp" file.
- */
-
 struct MoviesView: View {
-    
-    // INSTANCE VARIABLES
-    
-    /**
-        OVERALL APP STATE (@State)
-        - Track if RatingsView is showing ( Bool )
-        - Store all Ratings ( [Movie] )
-     */
-    
-    // ADD CODE HERE
-    
-    /**
-        MOVIES VIEW STATE
-        - Current movie object being shown ( Movie )
-        - Figure out a way to keep track of the current movie observed - from Movie.allMovies
-     */
-    
-    // ADD CODE HERE
-    
-    /**
-        CURRENT RATING STATE
-        - Rating of current movie ( Int )
-        - Written review of current movie ( String )
-        ( Reset these when rating is submitted or skipped. )
-     */
-    
-    // ADD CODE HERE
-    
-    
-    /**
-      YOUR UI
-     - All UI code belongs in the body variable.
-     - The outermost view should be a layout View (i.e. VStack, HStack, ScrollView) with multiple subviews.
-     */
-    var body: some View {
-        Text("YOUR UI CODE HERE") // REPLACE WITH YOUR CODE
-    }
-    
-    func getNextMovie() {
-        /**
-        - Jump to next movie in Movie.allMovies and update state accordingly.
-        - BONUS: Update the UI with an animation of your choice.
-         */
-        
-        // ADD CODE HERE
-    }
-    
-    func submitRating() {
-        /**
-         - Add the current movie rating state to some collection of all reviews made.
-         - Jump to to next movie.
-         */
-        
-        // ADD CODE HERE
-    }
-}
+    @State private var movies = Movie.allMovies
+    @State private var currentIndex = 0
+    @State private var rating: Int? = nil
+    @State private var review: String = ""
+    @State private var showRatingsView = false
 
-#Preview {
-    MoviesView()
+    var body: some View {
+        // UI Code Goes Here
+    }
+
+    private func getNextMovie() {
+        if currentIndex < movies.count - 1 {
+            currentIndex += 1
+        }
+    }
+
+    private func submitRating() {
+        guard currentIndex < movies.count else { return }
+        var movie = movies[currentIndex]
+        movie.rating = rating
+        movie.myReview = review
+        movies[currentIndex] = movie
+        getNextMovie()
+    }
 }
